d8a17ec0731ef3a830b36e826ea31c1693191ea6
Eadweard <804875728@qq.com>
2023-09-15 10:25:04 +0800

    webpack配置

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+// webpack.config.js
+
+const path = require('path');
+const WebpackDevServer = require('webpack-dev-server');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js', // 入口文件
+  plugins: [
+    new HtmlWebpackPlugin({
+     title: 'Development',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js', // 输出文件名
+    path: path.resolve(__dirname, 'dist'), // 输出目录
+    clean: true, // 清理dist文件夹
+  },
+  
+  devServer: {
+    static: './dist',
+    port: 9000,
+    host: 'localhost',
+    hot: true,
+    open: true
+  },
+  devtool: 'source-map',
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
