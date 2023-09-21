9c9349ed9584e64bd9b1178d261161bfe7509bbb
Eadweard <804875728@qq.com>
2023-09-21 11:35:44 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ import AudioPlay from './lib/audioPlay'
 window.onload = function () {
   new SpeechRecognitionLocale()
   const audio = new AudioPlay()
-  setTimeout(() => {
+  window.addEventListener('online', () => {
+    console.log('网络已连接')
+  })
+  window.addEventListener('offline', () => {
+    console.log('网络已断开')
+  })
+  setInterval(() => {
     audio.play()
   }, 5000)
 }
